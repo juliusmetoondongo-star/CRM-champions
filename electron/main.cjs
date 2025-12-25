7d8d0a691e585dee1471de0014f21531506efb0f
Julius Meto'o Ndongo <juliusmetoondongo@mac.home>
2025-12-25 22:42:32 +0100

    Package Electron from dist (no Vite build)

--- electron/main.cjs
+++ electron/main.cjs
@@ -1,8 +1,6 @@
 const { app, BrowserWindow } = require("electron");
 const path = require("path");
 
-const isDev = !app.isPackaged;
-
 function createWindow() {
   const win = new BrowserWindow({
     width: 1200,
@@ -14,17 +12,11 @@ function createWindow() {
     },
   });
 
-  if (isDev) {
-    win.loadURL("http://localhost:5173");
-    win.webContents.openDevTools();
-  } else {
-    win.loadFile(path.join(__dirname, "../dist/index.html"));
-  }
+  win.loadFile(path.join(__dirname, "../dist/index.html"));
 }
 
 app.whenReady().then(() => {
   createWindow();
-
   app.on("activate", () => {
     if (BrowserWindow.getAllWindows().length === 0) createWindow();
   });
